1dd14f3a9afb6f99364708e4a3577321d3e1f391
Yaqi Zhang <zhang623@wisc.edu>
2018-11-29 18:10:14 -0600

    win Bloomberg CodeCon first prize

---
+++ contests/Bloomberg-Code-Con/temp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+##################################
+# University of Wisconsin-Madison
+# Author: Yaqi Zhang
+##################################
+
+
+
+if __name__ == "__main__":
+    row = list(map(int, input().split()))
+    print(row)
